36bf6b9e15fb98920fcfd328438a40e55bb029ec
Iminsioluwa Adedunmoye <iminsioluwadedunmoye23@gmail.com>
2024-09-16 17:34:42 +0100

    Iminsi

---
+++ index.js
@@ -0,0 +1,24 @@
+var allStudents = ['Habbeb', 'Abdullateef', 'Winner', 'Rebecca', 'John']
+console.log(allStudents[3]);
+
+var evenNumbers = [2,4,6,8,10,]
+
+
+
+var cart = []
+cart.push('Corn Flakes')
+cart.unshift('Milo')
+cart.push('Ipara')
+cart.unshift('Body Spray')
+cart.push('Indomie')
+cart.push('Oats')
+cart.pop()
+cart.push('5 alive')
+cart.shift()
+cart.unshift('Active')
+cart.push('pulpy')
+console.log(cart);
+
+
+console.log(cart.length);
+
